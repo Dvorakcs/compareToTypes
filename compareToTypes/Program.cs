0d507f916661edf7a012977b56ed13014a30c176
Marcelo <mshf15@hotmail.com>
2023-08-30 10:13:50 -0300

    1

--- compareToTypes/Program.cs
+++ compareToTypes/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using compareToTypes;
+
+public class  Program
+{
+    public static int Main(string[] args)
+    {
+        var usuarioMemory = new UsuarioModel(1, "Marcelo", 21, 1.86);
+        var listUsuarioMemory = new List<UsuarioModel>()
+        {
+            new UsuarioModel(1,"teste",21,1.98),
+            new UsuarioModel(2,"teste2",22,1.98),
+            new UsuarioModel(5,"teste4",21,1.98)
+        };
+        Console.WriteLine(listUsuarioMemory.GetType() == typeof(List<UsuarioModel>));
+        Console.WriteLine(usuarioMemory.GetType() == typeof(UsuarioModel));
+        Console.WriteLine(usuarioMemory.ToString());
+        Console.WriteLine(usuarioMemory.GetType().ToString());
+        return 1;
+    }
+}
---
+++ compareToTypes/UsuarioModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareToTypes
+{
+    internal class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double height { get; set; }
+
+        public UsuarioModel(int id, string name, int age, double height)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            this.height = height;
+        }
+    }
+}
